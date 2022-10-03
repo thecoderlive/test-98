f52f54265113563882936c48faa84e5036fb552a
The Coder <82015917+thecoderlive@users.noreply.github.com>
2022-10-03 20:44:59 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,145 @@
+export const item = {
+    "id": "3d9abf43-a3c9-48f3-9f3a-f9c0e2cf4b40",
+    "home_feed": [
+        {
+            "id": "31a503be-4ed0-481f-9363-4cb11dc0b157",
+            "artist_name": "response",
+            "home_carousel": [
+                {
+                    "id": "d480ff70-9295-41e9-b481-abb7290c5631",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/290.png"
+                },
+                {
+                    "id": "b0216cb7-03cb-49a8-bb91-75287c8ec4ad",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/364.png"
+                },
+                {
+                    "id": "eb0734da-6518-430c-b1af-575e8b2e4525",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/155.png"
+                },
+                {
+                    "id": "62e8c527-3fcb-4f04-8874-e07f6fa7fbfe",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/93.png"
+                },
+                {
+                    "id": "266dcf6f-4f65-4912-bdd7-e015dbc9fc01",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/263.png"
+                }
+            ],
+            "art_caption": "talk",
+            "art_description": "Create try crime article. Line easy once term act."
+        },
+        {
+            "id": "62bb9888-5591-4c2e-8fd2-8eae665da0a1",
+            "artist_name": "the",
+            "home_carousel": [
+                {
+                    "id": "120ceeda-9a2f-48fb-8e53-53666383b22d",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/360.png"
+                },
+                {
+                    "id": "31489f1e-b4de-4da8-8477-412c2beaa11f",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/5.png"
+                },
+                {
+                    "id": "6bbb0663-6339-49ae-9298-85fad24a037b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/78.png"
+                },
+                {
+                    "id": "d4dfa73d-0fb9-4f61-b53e-7530ae70b224",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/188.png"
+                },
+                {
+                    "id": "4ebc2b0c-82e0-4c24-b76f-13d77370788b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/333.png"
+                }
+            ],
+            "art_caption": "arrive",
+            "art_description": "Including eight face budget house manage. Environment soldier election TV task herself."
+        },
+        {
+            "id": "141a8f69-76aa-40d3-bb29-6b55d19206c8",
+            "artist_name": "talk",
+            "home_carousel": [
+                {
+                    "id": "b5466634-1e97-459b-9962-c293b255c5f4",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/73.png"
+                },
+                {
+                    "id": "17c342e1-990d-42a5-9309-4e0bd0b7d401",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/35.png"
+                },
+                {
+                    "id": "ea7a7e5a-5842-4995-8632-c840c37e4779",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/313.png"
+                },
+                {
+                    "id": "9c97d5b8-e3ba-4f09-bd02-012a59665ff3",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/269.png"
+                },
+                {
+                    "id": "e7ad5651-1317-4b3e-8d54-b60a3172c438",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/330.png"
+                }
+            ],
+            "art_caption": "option",
+            "art_description": "Them weight wide we sign probably over. Laugh make peace mention too. Dark sound president person."
+        },
+        {
+            "id": "d408c5b5-2c45-49d7-95a0-b6ecb36b41d2",
+            "artist_name": "around",
+            "home_carousel": [
+                {
+                    "id": "791442de-7557-407f-b389-de2a89acd27e",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/290.png"
+                },
+                {
+                    "id": "83b84585-8c24-41a3-99ef-010cdeb046c5",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/213.png"
+                },
+                {
+                    "id": "50c60c82-5ca4-438f-a54c-9f59ecf36e65",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/134.png"
+                },
+                {
+                    "id": "c1c77994-e032-4d08-b438-83eb27f49e76",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/40.png"
+                },
+                {
+                    "id": "9b2455c9-8f2a-49e4-a0f6-850496788ca1",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/304.png"
+                }
+            ],
+            "art_caption": "paper",
+            "art_description": "Right tree fire early drug theory. Response parent section type kind. Yourself develop example about fact."
+        },
+        {
+            "id": "eb8fd405-512d-4391-be87-c93fa273e66c",
+            "artist_name": "situation",
+            "home_carousel": [
+                {
+                    "id": "95d7e2b9-b296-4da9-a375-2aa26172b7c8",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/178.png"
+                },
+                {
+                    "id": "429e31fe-9dc1-4b4e-88bb-3e75e2a008c3",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/249.png"
+                },
+                {
+                    "id": "081b342a-801b-4256-9818-abbde9517940",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/51.png"
+                },
+                {
+                    "id": "e955e388-da4f-4cbe-becb-89bed0c120f0",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/353.png"
+                },
+                {
+                    "id": "7a9e1309-693e-4cfc-a855-de3dc17b8d27",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/274.png"
+                }
+            ],
+            "art_caption": "energy",
+            "art_description": "Try talk federal open. Behind common unit collection everyone identify federal with. So artist somebody affect that only safe."
+        }
+    ]
+}
